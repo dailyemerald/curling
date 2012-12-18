bd999cd6b7a446b7a85dd5ea9cbf1925b8ac7224
Ivar Vong <ivar@ivarvong.com>
2012-12-18 11:58:52 -0800

    read urls

--- curl.rb
+++ curl.rb
@@ -1,2 +1,6 @@
-urls = File.open("urls.txt").readlines()
-p urls
+p `git reset --hard && git pull`
+File.open("urls.txt").readlines().each do |url|
+  url = url.strip
+  p "#{url} #{open(url).read.length}"
+end
+p "---"
